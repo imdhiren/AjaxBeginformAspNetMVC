752a4b26e881206cd8e52e238c3c4fbd69756fbe
Dhiren Patel <Dhiren Patel>
2017-12-11 19:24:08 +0530

    Add project files.

---
+++ AjaxBeginformAspNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using AjaxBeginformAspNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxBeginformAspNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [HandleError]
+        public ActionResult Add(Employee empmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Sucess Message";
+                ModelState.Clear();
+                return PartialView("__EmployeePartial");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Failure Message";
+            }
+            return PartialView("__EmployeePartial", empmodel);
+        }
+    }
+}
---
+++ AjaxBeginformAspNetMVC/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AjaxBeginformAspNetMVC.Models
+{
+    public class Employee
+    {
+        public int? Id { get; set; }
+        [Display(Name = "FirstName")]
+        [Required(ErrorMessage = "FirstName is Required.")]
+        public string FirstName { get; set; }
+        [Display(Name = "LastName")]
+        [Required(ErrorMessage = "LastName is Required.")]
+        public string LastName { get; set; }
+        [Display(Name = "Salary")]
+        [Required(ErrorMessage = "Salary is Required.")]
+        public int Salary { get; set; }
+    }
+}
